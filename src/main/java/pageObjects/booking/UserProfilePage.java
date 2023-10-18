d6f45bf7e53125a5bf239a638dce5531312495d6
anastasia <sekach.199855@gmail.com>
2023-10-18 15:09:39 +0300

    added visibility condition for given_name

--- src/main/java/pageObjects/booking/UserProfilePage.java
+++ src/main/java/pageObjects/booking/UserProfilePage.java
@@ -13,10 +13,10 @@ public class UserProfilePage {
 
     public void changeUsername(String newName, String newLastName) {
         $x("//button[@data-ga-label='Edit section: name']").click();
-        SelenideElement username = $x("//input[@autocomplete='given-name']");
+        SelenideElement username = $x("//input[@autocomplete='given-name']").shouldBe(Condition.visible);
         username.doubleClick();
         username.sendKeys(newName);
-        SelenideElement lastName = $x("//input[@autocomplete='family-name']");
+        SelenideElement lastName = $x("//input[@autocomplete='family-name']").shouldBe(Condition.visible);
         lastName.doubleClick();
         lastName.sendKeys(newLastName);
         $x("//button[@data-test-id='mysettings-btn-save']").click();
